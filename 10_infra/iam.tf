548001c5a8031dbe2db690b660a8b604f3635d73
hkobashi <71940017+hkobashi@users.noreply.github.com>
2023-04-17 13:14:29 +0900

    ednt resource name

--- 10_infra/iam.tf
+++ 10_infra/iam.tf
@@ -5,8 +5,8 @@
 # app-iam-role
 # ---------------------------------------------
 resource "aws_iam_instance_profile" "app_ec2_profile" {
-  name = aws_iam_role.app_iam_role.name
-  role = aws_iam_role.app_iam_role.name
+  name = aws_iam_role.new_app_iam_role.name
+  role = aws_iam_role.new_app_iam_role.name
 }
 
 resource "aws_iam_role" "new_app_iam_role" {
@@ -32,23 +32,23 @@ data "aws_iam_policy_document" "ec2_assume_role" {
   }
 }
 
-resource "aws_iam_role_policy_attachment" "app_iam_role_ec2_readonly" {
-  role       = aws_iam_role.app_iam_role.name
+resource "aws_iam_role_policy_attachment" "new_app_iam_role_ec2_readonly" {
+  role       = aws_iam_role.new_app_iam_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ReadOnlyAccess"
 }
 
-resource "aws_iam_role_policy_attachment" "app_iam_role_ssm_managed" {
-  role       = aws_iam_role.app_iam_role.name
+resource "aws_iam_role_policy_attachment" "new_app_iam_role_ssm_managed" {
+  role       = aws_iam_role.new_app_iam_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
-resource "aws_iam_role_policy_attachment" "app_iam_role_ssm_readonly" {
-  role       = aws_iam_role.app_iam_role.name
+resource "aws_iam_role_policy_attachment" "new_app_iam_role_ssm_readonly" {
+  role       = aws_iam_role.new_app_iam_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMReadOnlyAccess"
 }
 
-resource "aws_iam_role_policy_attachment" "app_iam_role_s3_readonly" {
-  role       = aws_iam_role.app_iam_role.name
+resource "aws_iam_role_policy_attachment" "new_app_iam_role_s3_readonly" {
+  role       = aws_iam_role.new_app_iam_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
 }
 
